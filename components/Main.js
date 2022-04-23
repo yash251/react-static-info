852cce49d41eaa29cdda54395f413f3c9a5c0ce0
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-23 16:01:29 +0530

    add className

--- components/Main.js
+++ components/Main.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Main() {
     return (
         <main>
-            <h1>Fun facts about React</h1>
-            <ul>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
